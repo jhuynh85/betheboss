591c6127d9ecc17d33a6afc7a9073d6e15101167
Philippe Recto <precto1285@gmal.com>
2019-04-24 15:11:31 -0700

    adjusted Header area to match template

--- src/components/landingPage/HeaderLogo.js
+++ src/components/landingPage/HeaderLogo.js
@@ -5,15 +5,14 @@ import './HeaderImg.css';
 const HeaderLogo = () => {
   return (
     <div className="JumbotronImg py-5 mt-0">
-      <div className=' row mx-5 px-5'>
-        <div className="col-md-6 p-5">
-          <img className="idea" src={Business} alt='icon' width="100%" height="100%" />
+      <div className='row mt-5 pt-5'>
+        <div className="col-md-6 text-center">
+          <img className="idea" src={Business} alt='icon' width="80%" height="80%" />
         </div>
-        <div className="col-md-6 pt-5 pr-5">
-          <div className="text-white text-center">
-            <h1 >BE THE BOSS</h1>
-            <h3>Passionpreneurship Program</h3>
-
+        <div className="col-md-6 mt-5">
+          <div className="text-white text-left mt-5 pl-5">
+            <h1 className="">BE THE BOSS</h1>
+            <h3 className="">Passionpreneurship Program</h3>
           </div>
         </div>
       </div>
