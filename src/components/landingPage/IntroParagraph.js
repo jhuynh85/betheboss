a872191924c68319a1227fb5038bf96194313b58
Philippe Recto <precto1285@gmal.com>
2019-04-24 15:28:18 -0700

    added About Us title to intro paragraph

--- src/components/landingPage/IntroParagraph.js
+++ src/components/landingPage/IntroParagraph.js
@@ -6,12 +6,13 @@ const IntroParagraph = () => {
   return (
     <Card>
       <div className="card-body p-4">
+        <h5>About Us:</h5>
         <p>Our Be The Boss Passionpreneurship Program transforms youth and young adults (ages 16-24) into confident and successful  entrepreneurs. </p>
 
         <p>Through our six-week program, interns generate business ideas, conduct market research, write business plans, pitch to a panel of investors, and launch their very own businesses.</p>
       </div>
       <div className="card-footer pt-5">
-      <center><h4>Brought To You By:</h4></center>
+        <center><h4>Brought To You By:</h4></center>
         <center><img src={Sdth} alt='sdth' width="80%" height="80%" /></center>
       </div>
     </Card>
