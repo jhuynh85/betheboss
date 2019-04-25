9cd987bf99b9c714426dfd0d1556cf68e3cdb423
Philippe Recto <precto1285@gmal.com>
2019-04-25 11:12:42 -0700

    removed import card from AboutUs.js component

--- src/components/landingPage/AboutUs.js
+++ src/components/landingPage/AboutUs.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Card } from 'reactstrap';
 
 const AboutUs = () => {
   return (
     <div>
-    <h3 className="text-center">Our Be the Boss Internship Program</h3>
-          <p>Our team of professional leaders and community mentors transform youth and young adults (ages 16-24) into confident and successful entrepreneurs.</p>
-          
-          <p>Through our six-week program, interns generate business ideas, conduct market research, write business plans, pitch to a panel of investors, and launch their very own businesses.</p>
-      
+      <h3 className="text-center">Our Be the Boss Internship Program</h3>
+      <p>Our team of professional leaders and community mentors transform youth and young adults (ages 16-24) into confident and successful entrepreneurs.</p>
+
+      <p>Through our six-week program, interns generate business ideas, conduct market research, write business plans, pitch to a panel of investors, and launch their very own businesses.</p>
+
     </div>
   );
 };
