0223ea5503dbb6b68739e34d515142fd58981baa
Philippe Recto <precto1285@gmal.com>
2019-04-25 00:28:08 -0700

    updated content for Attribution

--- src/components/CodeOfConduct.js
+++ src/components/CodeOfConduct.js
@@ -90,10 +90,10 @@ const CodeOfConduct = () => (
 
       <h2>Attribution</h2>
 
-      <p>This Code of Conduct is adapted from the [Contributor Covenant][homepage], version 1.4,
+      <p>This Code of Conduct is adapted from the Contributor Covenant, version 1.4,
     available at: <br /> <span><a href="https://www.contributor-covenant.org/version/1/4/code-of-conduct.html" target="_blank">www.contributor-covenant.org/version/1/4/code-of-conduct.html</a></span></p>
 
-      <p>[homepage]: <br /><span><a href="https://www.contributor-covenant.org" target="_blank">www.contributor-covenant.org</a></span></p>
+      <p>Homepage: <br /><span><a href="https://www.contributor-covenant.org" target="_blank">www.contributor-covenant.org</a></span></p>
 
       <p>For answers to common questions about this code of conduct, see:<br />
         <span><a href="https://www.contributor-covenant.org/faq" target="_blank">www.contributor-covenant.org/faq</a></span></p>
