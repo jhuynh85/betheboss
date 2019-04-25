88bc496d4bff47bcc70a014e3fe7693c2c79c92b
Philippe Recto <precto1285@gmal.com>
2019-04-25 00:17:32 -0700

    adjusted logo location on header

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -4,7 +4,7 @@ import Img from "gatsby-image"
 import styled from "styled-components"
 
 const Header = ({ logo }) => (
-  <Nav>
+  <Nav className="pl-5">
     <Link to="/">
       <Img fluid={logo} style={{ width: "7rem" }} />
     </Link>
