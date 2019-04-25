b491f6270721570da698a53d491ff1ebb15be1e7
Philippe Recto <precto1285@gmal.com>
2019-04-25 00:10:52 -0700

    added checkbox to form

--- src/components/landingPage/SubscriptionForm.js
+++ src/components/landingPage/SubscriptionForm.js
@@ -5,7 +5,7 @@ import Logo from '../../images/bethebossiconTransparent.png';
 const SubscriptionForm = (props) => {
   return (
     <div>
-    <center><img src={Logo} alt='logo' height="50%" width="50%"/></center>
+      <center><img src={Logo} alt='logo' height="50%" width="50%" /></center>
       <Form>
         <FormGroup >
           <Label for="firstName">First Name:</Label>
@@ -15,9 +15,21 @@ const SubscriptionForm = (props) => {
           <Label for="lastName">Last Name:</Label>
           <Input type="text" id="lastName" />
         </FormGroup>
-        <FormGroup>
+        <FormGroup email>
           <Label for="email">Email:</Label>
           <Input type="email" id="email" />
+        </FormGroup>
+        <FormGroup check>
+          <Label check>
+            <Input type="checkbox" />{' '}
+            I would like to receive updates regarding your program
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+          <Label check>
+            <Input type="checkbox" />{' '}
+            I would like to learn how to support your program
+          </Label>
         </FormGroup>
       </Form>
     </div>
