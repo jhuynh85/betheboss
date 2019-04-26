360728cf0d6ab0c7e7f5dacd583d44471f12c616
Philippe Recto <precto1285@gmal.com>
2019-04-26 10:04:16 -0700

    added Passion photo to landing page

--- src/components/landingPage/AboutUs.js
+++ src/components/landingPage/AboutUs.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Passion from '../../images/be-the-boss-passion.jpg';
 
 const AboutUs = () => {
   return (
-    <div>
+    <div className="mt-3">
       <h3 className="text-center">Our Be the Boss Internship Program</h3>
       <p>Our team of professional leaders and community mentors transform youth and young adults (ages 16-24) into confident and successful entrepreneurs.</p>
 
       <p>Through our six-week program, interns generate business ideas, conduct market research, write business plans, pitch to a panel of investors, and launch their very own businesses.</p>
-
+      <br />
+<div  className="text-center">
+      <img src={Passion} alt="passion" width="60%" height="60%"/>
+    </div>
     </div>
   );
 };
