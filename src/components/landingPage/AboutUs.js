0141e8359d7d93357d213e9570fbaa1cf2e21145
Philippe Recto <precto1285@gmal.com>
2019-04-26 23:22:24 -0700

    adjusted carousel

--- src/components/landingPage/AboutUs.js
+++ src/components/landingPage/AboutUs.js
@@ -7,18 +7,15 @@ const AboutUs = () => {
     <div className="mt-2 px-1">
       <h3 className="text-center">Our Be the Boss Internship Program</h3>
       <hr />
-      <Row>
-        <Col className="col-md-6">
-          <p>Our team of professional leaders and community mentors transform youth and young adults (ages 16-24) into confident and successful entrepreneurs.</p>
 
-          <p>Through our six-week program, interns generate business ideas, conduct market research, write business plans, pitch to a panel of investors, and launch their very own businesses.</p>
-        </Col>
-        <Col className="col-md-6">
-          <div className="text-center">
-            <PhotoSlider />
-          </div>
-        </Col>
-      </Row>
+      <p>Our team of professional leaders and community mentors transform youth and young adults (ages 16-24) into confident and successful entrepreneurs.</p>
+
+      <p>Through our six-week program, interns generate business ideas, conduct market research, write business plans, pitch to a panel of investors, and launch their very own businesses.</p>
+
+      <div>
+        <PhotoSlider />
+      </div>
+
     </div>
   );
 };
