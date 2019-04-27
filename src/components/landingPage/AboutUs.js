bd96ad367ada199c40d34d86e6affe928f5797ba
Philippe Recto <precto1285@gmal.com>
2019-04-26 18:57:24 -0700

    adjusted location of carousel photos

--- src/components/landingPage/AboutUs.js
+++ src/components/landingPage/AboutUs.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import PhotoSlider from './PhotoSlider'
-import Passion from '../../images/be-the-boss-passion.jpg';
+import { Row, Col } from 'reactstrap'
 
 const AboutUs = () => {
   return (
-    <div className="mt-3">
+    <div className="mt-2 px-1">
       <h3 className="text-center">Our Be the Boss Internship Program</h3>
-      <p>Our team of professional leaders and community mentors transform youth and young adults (ages 16-24) into confident and successful entrepreneurs.</p>
+      <hr />
+      <Row>
+        <Col className="col-md-6">
+          <p>Our team of professional leaders and community mentors transform youth and young adults (ages 16-24) into confident and successful entrepreneurs.</p>
 
-      <p>Through our six-week program, interns generate business ideas, conduct market research, write business plans, pitch to a panel of investors, and launch their very own businesses.</p>
-      <br />
-      <div className="text-center">
-        <PhotoSlider />
-      </div>
+          <p>Through our six-week program, interns generate business ideas, conduct market research, write business plans, pitch to a panel of investors, and launch their very own businesses.</p>
+        </Col>
+        <Col className="col-md-6">
+          <div className="text-center">
+            <PhotoSlider />
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
